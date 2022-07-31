97874ce5da780c7bafb0a39fc7570c44245edd9c
selt-selene-dion <110159482+selt-selene-dion@users.noreply.github.com>
2022-08-01 01:58:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ sendall = 0
 
 # TODO 
 # replace the 0 with the pins channel ID for your sever
-pins_channel = 1003002734045298739
+pins_channel = 957915070589988874
 
 # TODO
 # add any black listed channel IDs as a list separated by a comma (,)
